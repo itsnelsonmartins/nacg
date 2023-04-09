333e6f53a7597109fd33d6ea5b2c48be25c7fa61
Nelson Martins <hi@itsnelsonmartins.com>
2023-04-09 18:51:49 +0200

    added minify to build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [react()],
     base: '/nacg/',
     build: {
+        minify: true,
         rollupOptions: {
             output: {
                 entryFileNames: `assets/[name].js`,
